f6673ad4e23770417cf6693ceaa296a4fc5bc562
iam-ogomide <ogomidesamuel10@gmail.com>
2025-03-06 13:18:54 +0100

    gallery-intro

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -12,4 +12,13 @@ export class GalleryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  packages = [
+    { name: 'Membership Organisations', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/in1.jpg' },
+    { name: 'National Associations', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/city.jpg' },
+    { name: 'Clubs And Groups', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/city1.jpg' },
+    { name: 'Membership Organisations', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/city2.jpg' },
+    { name: 'National Associations', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/im4.jpg' },
+    { name: 'Clubs And Groups', description: 'Our membership management software provides full automation of membership renewals and payments', green: '../../assets/im8.jpg' },
+  ];
+
 }
