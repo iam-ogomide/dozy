4caa9fa422acaf9f672bdbf1c6174fd12e7d585a
iam-ogomide <ogomidesamuel10@gmail.com>
2025-03-06 10:27:17 +0100

    About-100

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { VenturesComponent } from './ventures/ventures.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
     NavbarComponent,
     HomeComponent,
     FooterComponent,
-    AboutComponent
+    AboutComponent,
+    VenturesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ventures/ventures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ventures',
+  templateUrl: './ventures.component.html',
+  styleUrls: ['./ventures.component.css']
+})
+export class VenturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
